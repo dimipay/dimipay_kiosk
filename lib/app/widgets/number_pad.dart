547babb13dae1c5c088b4e3dd63f30ea3ec3e8e5
hayward2007 <hayward2007@naver.com>
2024-01-19 19:44:23 +0900

    Modified NumberPad design

--- lib/app/widgets/number_pad.dart
+++ lib/app/widgets/number_pad.dart
@@ -4,8 +4,8 @@ import 'package:material_symbols_icons/symbols.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-const double buttonHeight = 64;
-const double buttonWidth = 64;
+const double buttonHeight = 72;
+const double buttonWidth = 72;
 
 class NumberPadButton extends StatelessWidget {
   const NumberPadButton({required this.number, super.key});
@@ -21,13 +21,13 @@ class NumberPadButton extends StatelessWidget {
         behavior: HitTestBehavior.translucent,
         child: Obx(() {
           return Container(
-            height: buttonHeight,
-            width: buttonWidth,
+            height: 72,
+            width: 72,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
+              borderRadius: BorderRadius.circular(12),
               color: NumberController.to.pressed &&
                       NumberController.to.pressedNumber.contains(number)
-                  ? DPColors.grayscale500
+                  ? DPColors.grayscale200
                   : Colors.transparent,
             ),
             alignment: Alignment.center,
@@ -49,37 +49,41 @@ class NumberPad extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        constraints: const BoxConstraints(maxWidth: 280),
-        padding: const EdgeInsets.fromLTRB(
-            34 - buttonWidth * 0.5,
-            52 - buttonHeight * 0.5,
-            34 - buttonWidth * 0.5,
-            52 - buttonHeight * 0.5),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          for (int i = 0; i < NumberController.to.numbers.length - 1; i += 3)
-            Column(children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                for (int j = i; j < i + 3; j++)
-                  NumberPadButton(number: NumberController.to.numbers[j])
-              ]),
-              const SizedBox(height: 96 - buttonHeight)
-            ]),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+    return Wrap(
+      alignment: WrapAlignment.center,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      direction: Axis.vertical,
+      spacing: 64,
+      children: [
+        for (int i = 0; i < NumberController.to.numbers.length - 1; i += 3)
+          Wrap(spacing: 40, children: [
+            for (int j = i; j < i + 3; j++)
+              NumberPadButton(number: NumberController.to.numbers[j])
+          ]),
+        Wrap(
+          spacing: 40,
+          children: [
             const SizedBox(width: buttonWidth, height: buttonHeight),
             NumberPadButton(number: NumberController.to.numbers[9]),
             GestureDetector(
                 onTapDown: (_) => NumberController.to.down(10),
                 onTapUp: (_) => NumberController.to.up(10),
-                child: Container(
-                    height: buttonHeight,
-                    width: buttonWidth,
-                    color: NumberController.to.pressed &&
-                            NumberController.to.pressedNumber.contains(10)
-                        ? DPColors.grayscale500
-                        : Colors.transparent,
-                    child: const DPIcons(Symbols.backspace_rounded)))
-          ])
-        ]));
+                child: Obx(() {
+                  return Container(
+                      height: buttonHeight,
+                      width: buttonWidth,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: NumberController.to.pressed &&
+                                NumberController.to.pressedNumber.contains(10)
+                            ? DPColors.grayscale200
+                            : Colors.transparent,
+                      ),
+                      child: const DPIcons(Symbols.backspace_rounded));
+                }))
+          ],
+        )
+      ],
+    );
   }
 }
