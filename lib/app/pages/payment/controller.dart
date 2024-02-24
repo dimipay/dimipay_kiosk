bb75fb71820af82715e8b9655697f549d03ffcbd
hayward2007 <hayward2007@naver.com>
2024-02-24 12:54:39 +0900

    Payment Page Animation Added

--- lib/app/pages/payment/controller.dart
+++ lib/app/pages/payment/controller.dart
@@ -1,15 +1,114 @@
-import 'package:camera/camera.dart';
+import 'dart:async';
+
 import 'package:get/get.dart';
 
-import 'package:dimipay_kiosk/app/services/auth/service.dart';
+class Spot {
+  final double size;
+  final int color;
+  final RxDouble left;
+  final RxDouble top;
+  final Map<String, double> animatedPosition;
+
+  Spot({
+    required this.size,
+    required this.color,
+    required this.left,
+    required this.top,
+    required this.animatedPosition,
+  });
+}
 
 class PaymentPageController extends GetxController {
   static PaymentPageController get to => Get.find<PaymentPageController>();
 
-  CameraController? cameraController;
-  Rx<XFile?> imageFile = Rx(null);
+  final _turns = RxDouble(0.0);
+  final _backgroundSpot = <Spot>[
+    Spot(
+        size: 169.14,
+        color: 0xFFFF0000,
+        left: RxDouble(723.54 + 512),
+        top: RxDouble(25.41 - 169.14 - 512),
+        animatedPosition: {
+          'left': 723.54 - 249.12,
+          'top': 25.41,
+        }),
+    Spot(
+        size: 169.14,
+        color: 0xFFFF0000,
+        left: RxDouble(693.37 + 512),
+        top: RxDouble(1023.09 - 169.14 - 512),
+        animatedPosition: {
+          'left': 693.37 - 249.12,
+          'top': 1023.09,
+        }),
+    Spot(
+        size: 249.12,
+        color: 0xFF00C2FF,
+        left: RxDouble(759.52 + 512),
+        top: RxDouble(725.2 - 249.12 - 512),
+        animatedPosition: {
+          'left': 759.52 - 249.12,
+          'top': 725.2,
+        }),
+    Spot(
+        size: 249.12,
+        color: 0xFF00C2FF,
+        left: RxDouble(544.77 + 512),
+        top: RxDouble(425.58 - 249.12 - 512),
+        animatedPosition: {
+          'left': 544.77 - 249.12,
+          'top': 425.58,
+        }),
+    Spot(
+        size: 249.12,
+        color: 0xFF00FF7A,
+        left: RxDouble(778.22),
+        top: RxDouble(316.23 - 249.12),
+        animatedPosition: {
+          'left': 778.22 - 249.12,
+          'top': 316.23,
+        }),
+    Spot(
+        size: 250.96,
+        color: 0xFF4200FF,
+        left: RxDouble(361.67 + 512),
+        top: RxDouble(23.19 + 512),
+        animatedPosition: {
+          'left': 361.67 - 250.96,
+          'top': 23.19,
+        }),
+    Spot(
+        size: 328.0,
+        color: 0xFFFF7A00,
+        left: RxDouble(378.67 - 328 - 512),
+        top: RxDouble(798.71 + 512),
+        animatedPosition: {
+          'left': 378.67 - 328,
+          'top': 798.71,
+        }),
+  ];
 
-  Future<void> init() async {
-    AuthService.to.findUser();
+  get backgroundSpot => _backgroundSpot;
+  get turns => _turns.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    Timer(const Duration(milliseconds: 500), () {
+      for (var spot in _backgroundSpot) {
+        spot.left.value = spot.animatedPosition['left']!;
+        spot.top.value = spot.animatedPosition['top']!;
+      }
+      _turns.value++;
+      turnBackground();
+    });
+  }
+
+  void turnBackground() {
+    Timer(const Duration(milliseconds: 5000), () {
+      _turns.value++;
+      turnBackground();
+    });
   }
 }
--- lib/app/pages/payment/page.dart
+++ lib/app/pages/payment/page.dart
@@ -1,8 +1,28 @@
+import 'package:dimipay_design_kit/dimipay_design_kit.dart';
+import 'package:material_symbols_icons/symbols.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'dart:io';
 
 import 'package:dimipay_kiosk/app/pages/payment/controller.dart';
+
+class BackgroundSpot extends StatelessWidget {
+  const BackgroundSpot({super.key, required this.size, required this.color});
+
+  final double size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size * 1.01,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [BoxShadow(blurRadius: 400, color: color.withOpacity(0.7))],
+      ),
+    );
+  }
+}
 
 class PaymentPage extends GetView<PaymentPageController> {
   const PaymentPage({super.key});
@@ -10,30 +30,51 @@ class PaymentPage extends GetView<PaymentPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: SizedBox.expand(
-                child: FutureBuilder<void>(
-      future: PaymentPageController.to.init(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If the Future is complete, display the preview.
-          return Obx(() => PaymentPageController.to.imageFile.value != null
-              ? Image.file(File(PaymentPageController.to.imageFile.value!.path))
-              : const SizedBox());
-        } else {
-          // Otherwise, display a loading indicator.
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    )
-                // child: Stack(alignment: Alignment.center, children: [
-                // const PaymentBackground(),
-                // ClipRect(
-                //     child: BackdropFilter(
-                //         filter: ImageFilter.blur(sigmaX: 400, sigmaY: 400),
-                //         child: Container(color: Colors.transparent))),
-                // ]
-                // )
-                )));
+        body: SizedBox.expand(
+            child: Stack(
+      children: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("디미페이 앱의 결제 QR를 스캔해주세요",
+                  style: DPTypography.title(color: DPColors.grayscale1000)),
+              const SizedBox(height: 48),
+              Text("상품 스캔 창에서 결제 QR를 바로 스캔하면\n빠르게 결제를 완료할 수 있습니다",
+                  textAlign: TextAlign.center,
+                  style: DPTypography.header2(color: DPColors.grayscale600)),
+              const SizedBox(height: 67),
+              const SizedBox(height: 132),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const DPIcons(Symbols.arrow_back_ios_new,
+                      size: 24, color: DPColors.grayscale600),
+                  const SizedBox(width: 24),
+                  Text("상품 스캔 화면으로 돌아가기", style: DPTypography.header2())
+                ],
+              )
+            ],
+          ),
+        ),
+        Obx(() => AnimatedRotation(
+              turns: PaymentPageController.to.turns,
+              duration: const Duration(milliseconds: 5000),
+              curve: Curves.easeInQuad,
+              child: Stack(children: [
+                ...PaymentPageController.to.backgroundSpot.map((spot) {
+                  return AnimatedPositioned(
+                    left: spot.left.value,
+                    top: spot.top.value,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOutCubic,
+                    child: BackgroundSpot(
+                        size: spot.size, color: Color(spot.color)),
+                  );
+                }).toList()
+              ]),
+            ))
+      ],
+    )));
   }
 }
