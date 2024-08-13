6edb660d817de7d4ebbac9b42ccab25346bb65b1
hayward2007 <hayward2007@naver.com>
2024-08-13 22:16:32 +0900

    fix: deleted test code

--- lib/app/widgets/press.dart
+++ lib/app/widgets/press.dart
@@ -1,9 +1,28 @@
+// import 'package:get/get.dart';
+
+// mixin Press {
+//   final RxString _pressedButton = "".obs;
+
+//   // String get pressedButton => _pressedButton.value;
+
+//   pressButton(String button) {
+//     _pressedButton.value = button;
+//   }
+
+//   resetButton() {
+//     _pressedButton.value = "";
+//   }
+
+//   bool isPressed(String button) {
+//     return _pressedButton.value == button;
+//   }
+// }
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 mixin Press {
   final RxString _pressedButton = "".obs;
-
-  // String get pressedButton => _pressedButton.value;
 
   pressButton(String button) {
     _pressedButton.value = button;
@@ -15,5 +34,51 @@ mixin Press {
 
   bool isPressed(String button) {
     return _pressedButton.value == button;
+  }
+
+  String get pressedButton => _pressedButton.value;
+}
+
+class PressProvider extends InheritedWidget {
+  final Press press;
+
+  const PressProvider({
+    super.key,
+    required super.child,
+    required this.press,
+  });
+
+  static PressProvider? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<PressProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(PressProvider oldWidget) {
+    return oldWidget.press != press;
+  }
+}
+
+class PressWidget extends StatelessWidget with Press {
+  final Widget child;
+
+  PressWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return PressProvider(
+      press: this,
+      child: GestureDetector(
+        onTapDown: (_) {
+          if (isPressed("")) pressButton(key.toString());
+        },
+        onTapCancel: () {
+          resetButton();
+        },
+        onTapUp: (_) {
+          resetButton();
+        },
+        child: child,
+      ),
+    );
   }
 }
