f0dac776a3e5fec650341cc56e8f1e3a9526e026
Seungpyo Suh <me@sspzoa.io>
2024-08-18 20:20:52 +0900

    feat: add testpage

---
+++ lib/app/pages/test/page.dart
@@ -0,0 +1,47 @@
+import 'package:dimipay_kiosk/app/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  Widget linkToRoute(String route) {
+    return TextButton(
+      onPressed: () {
+        Get.toNamed(route);
+      },
+      child: Text(route),
+    );
+  }
+
+  Widget linkToRouteWithArgs(
+      String route, String title, Map<String, dynamic> args) {
+    return TextButton(
+      onPressed: () {
+        Get.toNamed(route, arguments: args);
+      },
+      child: Text(title),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Route Test Page"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          linkToRoute(Routes.ONBOARD),
+          linkToRoute(Routes.PIN),
+          linkToRoute(Routes.PAYMENT),
+          linkToRoute(Routes.PRODUCT),
+          linkToRoute(Routes.PAYMENT_SUCCESS),
+          linkToRoute(Routes.PAYMENT_FAILED),
+        ],
+      ),
+    );
+  }
+}
