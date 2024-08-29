21073949386ff2a5fe15da15c03745fd9894b44a
Seungpyo Suh <me@sspzoa.io>
2024-08-29 22:22:31 +0900

    fix: token refresh2

--- lib/app/provider/interceptors/jwt.dart
+++ lib/app/provider/interceptors/jwt.dart
@@ -7,7 +7,6 @@ import 'package:get/get.dart' hide Response;
 class JWTInterceptor extends Interceptor {
   final Dio _dioInstance;
 
-  // Dependency Injection
   JWTInterceptor(this._dioInstance);
 
   @override
@@ -15,55 +14,34 @@ class JWTInterceptor extends Interceptor {
     AuthService authService = Get.find<AuthService>();
 
     options.headers['Authorization'] ??=
-        'Bearer ${authService.jwt.token.accessToken}';
+    'Bearer ${authService.jwt.token.accessToken}';
 
     return handler.next(options);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    AuthService authService = Get.find<AuthService>();
-    //refresh api가 401시 무한 루프 방지
+    if (err.response?.statusCode == 401) {
+      AuthService authService = Get.find<AuthService>();
 
-    if (err.response == null ||
-        err.response?.requestOptions.path == '/auth/refresh') {
-      return handler.next(err);
-    }
+      if (err.requestOptions.path == '/kiosk/auth/refresh') {
+        return handler.next(err);
+      }
 
-    if (err.requestOptions.responseType == ResponseType.stream) {
       try {
         await authService.refreshAcessToken();
 
-        //api 호출을 다시 시도함
-        err.requestOptions.headers['Authorization'] = null;
-        final Response response = await _dioInstance.fetch(err.requestOptions);
+        err.requestOptions.headers['Authorization'] =
+        'Bearer ${authService.jwt.token.accessToken}';
+        final response = await _dioInstance.fetch(err.requestOptions);
         return handler.resolve(response);
-      } catch (e) {
-        //refresh 실패 시 401을 그대로 반환
+      } catch (refreshError) {
+        await authService.logout();
+        Get.offAllNamed(Routes.PIN);
         return handler.next(err);
       }
     }
 
-    DPHttpResponse httpResponse = DPHttpResponse.fromDioResponse(err.response!);
-
-    if (httpResponse.code == 'ERR_TOKEN_EXPIRED') {
-      try {
-        await authService.refreshAcessToken();
-
-        //api 호출을 다시 시도함
-        err.requestOptions.headers['Authorization'] = null;
-        final Response response = await _dioInstance.fetch(err.requestOptions);
-        return handler.resolve(response);
-      } catch (e) {
-        //refresh 실패 시 401을 그대로 반환
-        return handler.next(err);
-      }
-    }
-    if (httpResponse.message == '알 수 없는 사용자입니다.' ||
-        httpResponse.code == 'ERR_LOGINED_IN_OTHER_DEVICE') {
-      await authService.logout();
-      Get.offNamed(Routes.ONBOARDING);
-    }
     return handler.next(err);
   }
 }
