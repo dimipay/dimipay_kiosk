7e474be299bf675b8f674a8bf30e401cac985b4d
Seungpyo Suh <me@sspzoa.io>
2024-11-25 19:59:28 +0900

    fix: catch err with code

--- lib/app/provider/interceptors/jwt.dart
+++ lib/app/provider/interceptors/jwt.dart
@@ -3,6 +3,8 @@ import 'package:dimipay_kiosk/app/services/auth/service.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart' hide Response;
 
+import '../model/response.dart';
+
 class JWTInterceptor extends Interceptor {
   final Dio _dioInstance;
 
@@ -13,14 +15,21 @@ class JWTInterceptor extends Interceptor {
     AuthService authService = Get.find<AuthService>();
 
     options.headers['Authorization'] ??=
-    'Bearer ${authService.jwt.token.accessToken}';
+        'Bearer ${authService.jwt.token.accessToken}';
 
     return handler.next(options);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) async {
-    if (err.response?.statusCode == 401 || err.type == DioExceptionType.connectionTimeout) {
+    DPHttpResponse httpResponse = err.response!.toDPHttpResponse();
+
+    if (httpResponse.code == 'ERR_TOKEN_EXPIRED' ||
+        httpResponse.code == 'ERR_TOKEN_ERROR' ||
+        httpResponse.code == 'ERR_TOKEN_NOT_FOUND' ||
+        httpResponse.code == 'ERR_UNAUTHENTICATED' ||
+        httpResponse.code == 'ERR_WRONG_TOKEN_TYPE' ||
+        err.type == DioExceptionType.connectionTimeout) {
       AuthService authService = Get.find<AuthService>();
 
       if (err.requestOptions.path == '/kiosk/auth/refresh') {
@@ -31,7 +40,7 @@ class JWTInterceptor extends Interceptor {
         await authService.refreshAcessToken();
 
         err.requestOptions.headers['Authorization'] =
-        'Bearer ${authService.jwt.token.accessToken}';
+            'Bearer ${authService.jwt.token.accessToken}';
         final response = await _dioInstance.fetch(err.requestOptions);
         return handler.resolve(response);
       } catch (refreshError) {
@@ -46,5 +55,27 @@ class JWTInterceptor extends Interceptor {
   Future<void> _handleLogout(AuthService authService) async {
     await authService.logout();
     Get.offAllNamed(Routes.PIN);
+  }
+}
+
+extension DioResponseDTO on Response {
+  DPHttpResponse toDPHttpResponse() {
+    if (requestOptions.responseType == ResponseType.stream) {
+      return DPHttpResponse(
+        code: '',
+        message: '',
+        statusCode: 200,
+        timeStamp: '',
+        data: data,
+      );
+    }
+    return DPHttpResponse(
+      code: data['code'],
+      message: data['message'],
+      statusCode: data['statusCode'],
+      timeStamp: data['timestamp'],
+      data: data['data'],
+      errors: data['errors'],
+    );
   }
 }
